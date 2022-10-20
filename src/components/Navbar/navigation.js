7cd681135ff6ffe7305c1850f9496548a5a5a991
RohithKumarGanta <rohithkumarganta@gmail.com>
2022-10-20 05:46:57 +0000

    Tasty Kitchens

---
+++ src/components/Navbar/navigation.js
@@ -0,0 +1,10 @@
+import NavLinks from './navlinks'
+import classes from './index.css'
+
+const Navigation = () => (
+  <nav className={classes.Navigation}>
+    <NavLinks />
+  </nav>
+)
+
+export default navigation
